e7764a2d6fb32f2e5b6e72d5c74780cb47673298
VBeegle <VGBeegle@gmail.com>
2019-11-21 20:02:11 -0800

    Adds Fishhh home screen with working button

---
+++ Fishhh-prototype/Fishhh-prototype/ShopViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ShopViewController.swift
+//  Fishhh-prototype
+//
+//  Created by VictoriaGBeegle on 11/21/19.
+//  Copyright © 2019 VictoriaGBeegle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ShopViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Fishhh-prototype/Fishhh-prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fishhh-prototype
+//
+//  Created by VictoriaGBeegle on 11/21/19.
+//  Copyright © 2019 VictoriaGBeegle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
